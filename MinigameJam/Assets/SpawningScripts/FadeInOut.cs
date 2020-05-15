736f73430f13ba65de8fe25da6e229c2775aff1f
SoulSnorter <petrerobertcristian@gmail.com>
2020-05-15 23:51:20 +0300

    Update to spawner and fade in/out

---
+++ MinigameJam/Assets/SpawningScripts/FadeInOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    private Material mat;
+
+    void Start()
+    {
+        mat = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    void Update()
+    {
+        Color newColor = mat.color;
+        newColor.a -= Time.deltaTime * 0.2f;
+        mat.color = newColor;
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+    }
+}
